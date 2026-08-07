b8b8d54cf4d14db47afdc996cfde83a9a605e966
Jan <107766070+Jan-Stangelj@users.noreply.github.com>
2026-08-07 22:31:59 +0200

    Better settings for debugging

--- include/settings.hpp
+++ include/settings.hpp
@@ -6,7 +6,7 @@
 namespace rt {
     inline uint32_t minBounces = 2;
     inline uint32_t maxBounces = 16;
-    inline uint32_t samples = 128;
+    inline uint32_t samples = 32;
 
     inline float airIOR = 1.0f;
 
@@ -16,7 +16,7 @@ namespace rt {
     inline float DOFjiggle = 0.0f;
     inline float DOFfocus = 1.0f;
 
-    constexpr uint32_t resolutionX = 1280;
-    constexpr uint32_t resolutionY = 720;
+    constexpr uint32_t resolutionX = 800;
+    constexpr uint32_t resolutionY = 600;
     const std::string title = "Ray tracing in one weekend";
 }
